ea77abb83e91d7cc781a2777d53b9972759c97bd
Mehran Dhn <FloydianMehran@live.com>
2020-07-11 16:27:50 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObserverFXTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
